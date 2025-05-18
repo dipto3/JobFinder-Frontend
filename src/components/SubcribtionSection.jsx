0df98326966367476d6068a44c8a26b7f316d682
dipto <diptoguho4@gmail.com>
2025-05-18 12:27:04 +0600

    job category api implement

--- src/components/SubcribtionSection.jsx
+++ src/components/SubcribtionSection.jsx
@@ -1,4 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function SubcribtionSection() {
+  const [jobCategory, setJobCategory] = useState([]);
+  const [loadingJobCategory, setLoadingJobCategory] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setLoadingJobCategory(true);
+    async function getJobCategory() {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/api/job-categories`
+        );
+        // console.log(response.data.data);
+        if (response.status == 200) {
+          setJobCategory(response.data.data);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoadingJobCategory(false);
+      }
+    }
+    getJobCategory();
+  }, []);
   return (
     <>
       <section className="my-16 bg-white dark:bg-gray-800 p-8 rounded-xl shadow-sm">
@@ -21,9 +46,16 @@ export default function SubcribtionSection() {
               <div className="absolute inset-y-0 right-0 flex items-center">
                 <select className="h-full px-4 border-l border-gray-300 dark:border-gray-600 rounded-r-lg bg-gray-100 dark:bg-gray-700 dark:text-white focus:outline-none">
                   <option>All Categories</option>
+                  {jobCategory.length > 0 ? (
+                    jobCategory.map((item) => (
+                      <option value={item.id} key={item.id}>
+                        {item.name}
+                      </option>
+                    ))
+                  ) : (
+                    <p>Not Found</p>
+                  )}
                   <option>Tech</option>
-                  <option>Design</option>
-                  <option>Marketing</option>
                 </select>
               </div>
             </div>
